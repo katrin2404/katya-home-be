e5077164139f2bac8b40c6ea7fe4efda649168b7
katrin2404 <31765135+katrin2404@users.noreply.github.com>
2023-10-26 17:09:12 +0200

    Revert "Task5: integration with s3"

--- import-service/src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lambda-settings.constants';
-export * from './s3-operations.constants';
--- import-service/src/functions/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as importFileParser } from './importFileParser';
-export { default as importProductsFile } from './importProductsFile';
